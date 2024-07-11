443a1a5cbe42707756008995b2d176d0ceb88c3e
Nithish1206 <121179023+Nithish1206@users.noreply.github.com>
2024-07-11 11:46:48 +0530

    initial commit

---
+++ addingrows.cs
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+      int[,] arr={
+          {1,2,3},
+          {4,5,6},
+          {7,8,9}
+      };
+      for(int i=0;i<arr.GetLength(0);i++){
+          int temp=0;
+          for(int j=0;j<arr.GetLength(1);j++){
+              temp+=arr[i,j];
+          }
+          Console.WriteLine($"Sum of Row {i+1} : {temp}");
+      }
+  }
+}
---
+++ c-sharp.cs
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+      int[] arr1={10,2,6};
+      int[] arr2={9,3,5};
+      
+      int[] arr3= new int[arr1.Length + arr2.Length];
+      
+      int k=0;
+      foreach(int i in arr1){
+          arr3[k]=i;
+          k++;
+      }
+      
+      foreach(int i in arr2){
+          arr3[k]=i;
+          k++;
+      }
+      
+      Array.Sort(arr3);
+      foreach(int i in arr3){
+          Console.Write(i + " ");
+      }
+  }
+}
